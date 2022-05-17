5478adc51dbe845bb6c99692873585c9ac6d1bde
Thiago Galhardo <thiago.galhardo@hotmail.com>
2022-05-17 17:47:49 -0300

    change animations

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,71 @@
-ScrollReveal({
-    reset: true,
-    distance: "60px",
-    duration: 1000,
-    delay: 400
-});
+
+if (window.matchMedia("(min-width: 624px)").matches) {
+    /* a viewport tem pelo menos 624px de largura */
+    window.sr = ScrollReveal({
+        reset: true,
+        distance: '60px',
+        delay: 400
+    });
+} else {
+    /* a viewport menos que 624px de largura */
+    window.sr = ScrollReveal({
+        reset: true,
+        delay: 400,
+        duration: 900,
+    });
+
+}
 
 
-ScrollReveal().reveal(
-    `
+sr.reveal(`
 .s-hero .title, 
 .s-about,
-.about-me .my-picture,
-.form-contact,
-.copy
-`, {
-    delay: 300,
-    origin: 'top'
-
-}
+.about-me,
+.copy,
+.title-cases
+`, { delay: 300, origin: 'top' }
 );
+sr.reveal('header .container', { delay: 100, origin: 'top', distance: '5px' });
 
-ScrollReveal().reveal(
-    ` 
+sr.reveal(`
   .s-success .tag ,
   .title-success,
-  .cards,
-  .card,
-  .s-cases,
-  .title-cases,
-  .cases,
-  .title-contact`, {
-    delay: 400,
-    origin: 'left'
-
-}
+  .title-contact
+  `, { delay: 400, origin: 'left' }
 );
 
+sr.reveal(
+    `.cards,
+    `, { delay: 400, origin: 'right' }
+);
+
+sr.reveal('.input-group input, .input-group textarea', { delay: 600, origin: 'right', interval: 200, });
+sr.reveal('.cards', { delay: 600, origin: 'right', interval: 200, });
+sr.reveal('.input-button, .my-picture',
+    { delay: 1200, origin: 'right' }
+);
+
+
+sr.reveal(`
+ .about-me,
+ .swiper
+ `, { delay: 800, origin: 'bottom' }
+);
+
+sr.reveal('.techs a', { delay: 600, origin: 'bottom', interval: 200, });
 
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 3,
-    spaceBetween: 30,
+    spaceBetween: 10,
     loop: true,
     effect: "coverflow",
     grabCursor: true,
+    centeredSlides: true,
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
-        dynamicBullets: true,
     },
+
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
