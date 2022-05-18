914b992074f9510f2b6413b482625cfe0b69b77c
Thiago Galhardo <thiago.galhardo@hotmail.com>
2022-05-18 18:53:25 -0300

    add menu hamburger

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,17 +3,17 @@ if (window.matchMedia("(min-width: 624px)").matches) {
     /* a viewport tem pelo menos 624px de largura */
     window.sr = ScrollReveal({
         reset: true,
-        distance: '60px',
+        distance: '20px',
         delay: 400
     });
 } else {
-    /* a viewport menos que 624px de largura */
+    /* a viewport mais que 624px de largura */
     window.sr = ScrollReveal({
         reset: true,
         delay: 400,
-        duration: 900,
+        duration: 600,
+        distance: '5px',
     });
-
 }
 
 
@@ -22,10 +22,10 @@ sr.reveal(`
 .s-about,
 .about-me,
 .copy,
-.title-cases
+.title-projects
 `, { delay: 300, origin: 'top' }
 );
-sr.reveal('header .container', { delay: 100, origin: 'top', distance: '5px' });
+sr.reveal('s-hero', { delay: 100, origin: 'top', distance: '5px' });
 
 sr.reveal(`
   .s-success .tag ,
@@ -35,13 +35,12 @@ sr.reveal(`
 );
 
 sr.reveal(
-    '.cards, .hello-image img'
+    '.cards, .hello-image img, .form-contact, .input-button'
     , { delay: 400, origin: 'right' }
 );
 
-sr.reveal('.input-group input, .input-group textarea', { delay: 600, origin: 'right', interval: 200, });
 sr.reveal('.cards', { delay: 600, origin: 'right', interval: 200, });
-sr.reveal('.input-button, .my-picture',
+sr.reveal('.my-picture',
     { delay: 1200, origin: 'right' }
 );
 
@@ -84,3 +83,12 @@ var swiper = new Swiper(".mySwiper", {
 
     }
 });
+
+
+function toggleMenu() {
+    const menuToggle = document.querySelector('.toggle');
+    const sidebar = document.querySelector('.sidebar');
+    menuToggle.classList.toggle('active')
+    sidebar.classList.toggle('active')
+}
+
